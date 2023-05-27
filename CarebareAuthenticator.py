3ddd8f3e3cf17c1f1ff4dee27a699442cca7d25e
Garth Mortensen <mortensengarth@hotmail.com>
2023-05-26 21:47:53 -0500

    First push

---
+++ CarebareAuthenticator.py
@@ -0,0 +1,63 @@
+import subprocess
+
+
+class CarebareAuthenticator:
+    """
+    A class to handle Kerberos authentication.
+
+    Kerberos is a network authentication protocol. It uses tickets to provide identitifation over potentially insecure networks.
+	
+	A principal is a unique identity to which Kerberos can assign tickets. A principal could be a user/host/service, formatted as "primary/instance@REALM". 
+	
+    ...
+
+    Attributes
+    ----------
+    keytab : str
+        the path to the keytab file, which contains keypairs of principals:encrypted keys derived from the Kerberos password. They're used to authenticate to the Kerberos Key Distribution Center (KDC).
+    principal : str
+        the name of the principal
+
+    Methods
+    -------
+    keytab_kinit():
+        Authenticates using the keytab file.
+    """
+
+    def __init__(self, keytab: str, principal: str):
+        """
+        Constructs all the necessary attributes for the KerberosAuthenticator object.
+
+        Parameters
+        ----------
+            keytab : str
+                path to the keytab file
+            principal : str
+                name of the principal
+        """
+        self.keytab = keytab
+        self.principal = principal
+
+    def keytab_kinit(self) -> None:
+        """
+        Authenticates the principal using the keytab file.
+
+        kinit obtains and caches an initial ticket-granting ticket (TGT). With these credentials, users can authenticate to various network services.
+
+        Raises
+        ------
+        subprocess.CalledProcessError
+            If the kinit command fails.
+        """
+        try:
+            subprocess.run(["kinit", "-kt", self.keytab, self.principal], check=True)
+            print("kinit successful")
+        except subprocess.CalledProcessError as e:
+            print(f"kinit failed with error: {e}")
+
+
+keytab = "/path/to/your/keytab.file"
+principal = "your-principal@YOUR-REALM.COM"
+
+authenticator = CarebareAuthenticator(keytab, principal)
+authenticator.keytab_kinit()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CarebareAuthenticator
+
+In this version, I've added a simple explanation of Kerberos, principal, and the role of the `kinit` . This additional documentation should make the code more understandable for someone unfamiliar with Kerberos.
+
+I'm going to try to autodoc it.
